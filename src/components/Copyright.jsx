e13636e9d68254beda16a681d0937e4d2d6ca7b1
dm-mensavi <mensavidavid@gmail.com>
2023-10-14 21:07:06 +0000

    Add copyright section

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 function Copyright() {
   return (
-    <div>Copyright</div>
-  )
+    <div className="bg-gray-100 py-4 justify-start text-center">
+      <div className='flex justify-between flex-col sm:flex-row mx-auto w-10/12 px-1'>
+        <p className=" text-[#737373]/70 text-xs sm:text-sm font-semibold">
+          Made With Love By Zaptek Intern, All Right Reserved
+        </p>
+        <div className="flex space-x-5 mx-auto sm:mx-0 mt-2">
+          <FaFacebook size={20} className="text-orange-500" />
+          <FaInstagram size={20} className="text-orange-500" />
+          <FaTwitter size={20} className="text-orange-500" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Copyright
+export default Copyright;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { IoIosMail } from 'react-icons/io'
 
 function Footer() {
   return (
-    <div className="text-white py-10 px-6 md:px-10 lg:px-20">
+    <div className="text-white py-10 px-6 md:px-10 w-full sm:w-11/12 mx-auto lg:px-20">
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-16 sm:gap-6 md:gap-14">
         <div>
           <h3 className="text-[#252B42] font-bold mb-4">Company Info</h3>
