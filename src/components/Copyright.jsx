a4214bf29e1d68671880ee6a86e8963d0d81fb23
dm-mensavi <mensavidavid@gmail.com>
2023-10-14 21:13:21 +0000

    Change socila media target to blank

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -5,13 +5,19 @@ function Copyright() {
   return (
     <div className="bg-gray-100 py-4 justify-start text-center">
       <div className='flex justify-between flex-col sm:flex-row mx-auto w-10/12 px-1'>
-        <p className=" text-[#737373]/70 text-xs sm:text-sm font-semibold">
+        <p className="text-[#737373]/70 text-xs sm:text-sm font-semibold">
           Made With Love By Zaptek Intern, All Right Reserved
         </p>
         <div className="flex space-x-5 mx-auto sm:mx-0 mt-2">
-          <FaFacebook size={20} className="text-orange-500" />
-          <FaInstagram size={20} className="text-orange-500" />
-          <FaTwitter size={20} className="text-orange-500" />
+          <a href="https://www.facebook.com/" target='blank'>
+            <FaFacebook size={20} className="text-orange-500" />
+          </a>
+          <a href="https://www.instagram.com/" target='blank'>
+            <FaInstagram size={20} className="text-orange-500" />
+          </a>
+          <a href="https://twitter.com/" target='blank'>
+            <FaTwitter size={20} className="text-orange-500" />
+          </a>
         </div>
       </div>
     </div>
